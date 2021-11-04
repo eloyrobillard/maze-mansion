315775cd485e8bc14b26a12511647bdfee70bcfc
Eloy <eloy.robillard@gmail.com>
2021-11-04 07:38:08 +0100

    ✨ feat(asc): read str with __getString

--- client/src/recursive_backtracker/asc/test.js
+++ client/src/recursive_backtracker/asc/test.js
@@ -1,6 +1,6 @@
 const { add, getMaze, getArr } = require('./index').exports;
-const { __getArray } = require('./index').exports;
+const { __getArray, __getString } = require('./index').exports;
 
 console.log(add(1, 2));
-console.log(getMaze(10, 10));
+console.log(__getString(getMaze(10, 10)));
 console.log(__getArray(getArr()));
