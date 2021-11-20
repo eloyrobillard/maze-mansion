d82032db037f08c176e3f9f805c55c70602bcf76
Eloy <eloy.robillard@gmail.com>
2021-11-20 15:17:06 +0100

    ✨ feat(client ASC): add console.log for WASM

--- client/src/RBT/asc/index.ts
+++ client/src/RBT/asc/index.ts
@@ -2,6 +2,12 @@ import { instantiate } from "@assemblyscript/loader";
 import { WasmApi } from '../../AScApi';
 
 export default async function Asc() {
-  const module = await instantiate<WasmApi>(fetch('http://localhost:3001/maze/rbt/asc'));
+  const module = await instantiate<WasmApi>(fetch('http://localhost:3001/maze/rbt/asc'), {
+    console: {
+      log(msg: number) {
+        console.log(`msg: ${(msg && module.exports.__getString(msg)) || msg}`);
+      }
+    },
+  });
   return module.exports;
 };
