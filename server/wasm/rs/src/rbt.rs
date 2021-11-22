73047f5a8f1d68de0cafac78fc0d7da74bb7390f
Eloy <eloy.robillard@gmail.com>
2021-11-22 18:47:34 +0100

    ✨ feat(assembly | rust): complete recursive_backtracker

--- server/wasm/rs/src/rbt.rs
+++ server/wasm/rs/src/rbt.rs
@@ -1,10 +1,10 @@
-use crate::utils::get_neighbors;
+use crate::utils::*;
 
 use super::utils;
 
 pub fn recursive_backtracker(width: usize, height: usize) -> Vec<Vec<i32>> {
     let null = -1;
-    
+
     let mut y = -1;
     let mut x = -1;
     let mut grid: Vec<Vec<i32>> = Vec::with_capacity(height)
@@ -21,24 +21,36 @@ pub fn recursive_backtracker(width: usize, height: usize) -> Vec<Vec<i32>> {
         })
         .collect();
 
+    let mut cell_stack: Vec<i32> = Vec::new();
+
     let rng = fastrand::Rng::new();
     let x = rng.usize(0..width);
     let y = rng.usize(0..height);
+    let mut prev = null;
     let mut current = grid[y][x] + get_neighbors(&mut grid, x, y);
-    
+
+    cell_stack.push(current);
     let mut visited = 1;
 
-    let steps: Vec<Vec<i32>>;
+    let mut steps: Vec<Vec<i32>> = Vec::new();
 
     let toVisit = width * height;
-    let cell_stack: Vec<i32>;
-
 
     while visited < toVisit {
         if current == null {
+            current = cell_stack.pop().unwrap();
+        }
 
+        steps.push(vec![prev, current]);
+
+        prev = current;
+        current = get_next(current);
+        if current != null {
+            visited += 1;
         }
     }
+
+    steps.push(vec![prev, current]);
 
     grid
 }
