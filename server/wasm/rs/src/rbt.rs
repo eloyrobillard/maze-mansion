adfd7b57cfb5bf0601102d99c77faf8f0fdace2e
eloyrobillard <eloy.robillard@gmail.com>
2022-01-07 13:42:54 +0100

    🦄 refactor(rbt): have Maze own rbt

--- server/wasm/rs/src/rbt.rs
+++ server/wasm/rs/src/rbt.rs
@@ -11,11 +11,48 @@ pub struct Maze {
 #[wasm_bindgen]
 impl Maze {
     pub fn new(width: usize, height: usize) -> Maze {
-        let maze = recursive_backtracker(width, height);
+        let invalid = -1;
+
+        let mut grid: Vec<Vec<i32>> = init_grid(width, height);
+
+        let mut cell_stack: Vec<i32> = Vec::new();
+
+        let rng = fastrand::Rng::new();
+        let x = rng.usize(0..width);
+        let y = rng.usize(0..height);
+
+        let mut prev = invalid;
+        let mut current = grid[y][x] + get_neighbors(&mut grid, x, y);
+
+        cell_stack.push(current);
+        let mut visited = 1;
+
+        let mut steps: Vec<Vec<i32>> = Vec::new();
+
+        let to_visit = width * height;
+
+        while visited < to_visit {
+            if current == invalid {
+                current = cell_stack.pop().unwrap();
+            }
+
+            steps.push(vec![prev, current]);
+
+            prev = current;
+            current = get_next(current, &mut grid);
+            prev = grid[get_y(prev) as usize][get_x(prev) as usize];
+            if current != invalid {
+                visited += 1;
+                current |= get_neighbors(&mut grid, get_x(current), get_y(current));
+                cell_stack.push(current)
+            }
+        }
+
+        steps.push(vec![prev, current]);
         Maze {
-            init: maze[0].clone(),
-            steps: maze[1].clone(),
-            fin: maze[2].clone(),
+            init: init_grid(width, height),
+            steps,
+            fin: grid,
         }
     }
 
@@ -69,49 +106,4 @@ impl Maze {
         println!("{}", res);
         res
     }
-}
-
-pub fn recursive_backtracker(width: usize, height: usize) -> Vec<Vec<Vec<i32>>> {
-    let invalid = -1;
-    let mut res: Vec<Vec<Vec<i32>>> = vec![init_grid(width, height)];
-
-    let mut grid: Vec<Vec<i32>> = init_grid(width, height);
-
-    let mut cell_stack: Vec<i32> = Vec::new();
-
-    let rng = fastrand::Rng::new();
-    let x = rng.usize(0..width);
-    let y = rng.usize(0..height);
-
-    let mut prev = invalid;
-    let mut current = grid[y][x] + get_neighbors(&mut grid, x, y);
-
-    cell_stack.push(current);
-    let mut visited = 1;
-
-    let mut steps: Vec<Vec<i32>> = Vec::new();
-
-    let to_visit = width * height;
-
-    while visited < to_visit {
-        if current == invalid {
-            current = cell_stack.pop().unwrap();
-        }
-
-        steps.push(vec![prev, current]);
-
-        prev = current;
-        current = get_next(current, &mut grid);
-        prev = grid[get_y(prev) as usize][get_x(prev) as usize];
-        if current != invalid {
-            visited += 1;
-            current |= get_neighbors(&mut grid, get_x(current), get_y(current));
-            cell_stack.push(current)
-        }
-    }
-
-    steps.push(vec![prev, current]);
-    res.push(steps);
-    res.push(grid);
-    res
 }
