2ca63769ca27917784bc90fb61c0b766c6e646c8
eloyrobillard <eloy.robillard@gmail.com>
2022-02-01 11:15:44 +0100

    ✨ feat(rand.rs): use system time as seed

--- wasm/src/rand.rs
+++ wasm/src/rand.rs
@@ -1,3 +1,9 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 pub fn gen(max: usize) -> usize {
-  max / 2
+  let start = SystemTime::now();
+  let since_the_epoch = start
+      .duration_since(UNIX_EPOCH)
+      .expect("Time went backwards");
+  (since_the_epoch.as_millis() % max as u128).try_into().unwrap()
 }
