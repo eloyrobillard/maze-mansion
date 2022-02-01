7ea657fbe0e7a716c4eee91d6a168fd8c9c8344e
eloyrobillard <eloy.robillard@gmail.com>
2022-02-01 11:35:44 +0100

    🐞 fix(wasm.service): get instance & memory

--- src/Services/wasm.service.ts
+++ src/Services/wasm.service.ts
@@ -1,38 +1,27 @@
 import { MazeDescriptor, WASMApi } from "Types";
 
-let mod: WebAssembly.Module;
-let mem: WebAssembly.Memory;
-let instance: WebAssembly.Instance;
-
 const wasmImports = {
 	memoryBase: 0,
-	tableBase: 0,
 	memory: new WebAssembly.Memory({ initial: 256 }),
-	table: new WebAssembly.Table({ initial: 0, element: 'anyfunc' })
 };
 
 export const getApi = async (): Promise<WASMApi> => {
-	fetch('rs')
-		.then(response => response.arrayBuffer())
-		.then(bytes => WebAssembly.instantiate(bytes, { env: wasmImports }))
-		.then(results => {
-			return results.instance.exports;
-		});
-	let curMaze: MazeDescriptor/*  = maze.new(10, 10) */;
+	const {module, instance, memory} = await loadWasm();
+	let maze: MazeDescriptor;
 	return (() => {
 		return {
-			newMaze: (width: number, height: number) => curMaze/*  = maze.new(width, height) */,
-			getStepsLen: () => curMaze.get_steps_len(),
+			newMaze: (width: number, height: number) => maze.new(10, 10),
+			getStepsLen: () => maze.get_steps_len(),
 			generateClasses: () => {
 				// LINK https://stackoverflow.com/questions/47529643/how-to-return-a-string-or-similar-from-rust-in-webassembly
 				// LINK https://stackoverflow.com/questions/49014610/passing-a-javascript-string-to-a-rust-function-compiled-to-webassembly/49020435#49020435
-				const height = curMaze.get_height();
-				const width = curMaze.get_width();
-				const ptr = curMaze.get_final();
+				const height = maze.get_height();
+				const width = maze.get_width();
+				const ptr = maze.get_final();
 				console.log('class lists generated');
-				const classPtrs = Array.from(new Uint32Array(mem.buffer, ptr, height));
+				const classPtrs = Array.from(new Uint32Array(memory.buffer, ptr, height));
 				return classPtrs.map((p) =>
-					Array.from(new Uint32Array(mem.buffer, p, width)).map((s) => s + '')
+					Array.from(new Uint32Array(memory.buffer, p, width)).map((s) => s + '')
 				);
 			},
 
@@ -44,4 +33,20 @@ export const getApi = async (): Promise<WASMApi> => {
 			}
 		}
 	})();
+}
+
+async function loadWasm() {
+	let { module, instance } = await fetch('rs')
+		.then(response => response.arrayBuffer())
+		.then(bytes => WebAssembly.instantiate(bytes, { env: wasmImports }))
+		.then(results => {
+			console.log(results);
+			return results;
+		});
+	const memory = instance.exports.memory as WebAssembly.Memory;
+	return {
+		module, 
+		instance,
+		memory
+	}
 }
