4fbde2b9d5e79ddb03342927f8f493417bad1c6d
eloyrobillard <eloy.robillard@gmail.com>
2022-02-01 12:22:04 +0100

    ✨ feat(wasm.service): get fns from wasm

--- src/Services/wasm.service.ts
+++ src/Services/wasm.service.ts
@@ -6,18 +6,15 @@ const wasmImports = {
 };
 
 export const getApi = async (): Promise<WASMApi> => {
-	const {module, instance, memory} = await loadWasm();
-	let maze: MazeDescriptor;
+	const { module, exports, memory } = await loadWasm();
 	return (() => {
 		return {
-			newMaze: (width: number, height: number) => maze.new(10, 10),
-			getStepsLen: () => maze.get_steps_len(),
+			newMaze: (width: number, height: number) => exports.create_maze(10, 10),
+			getStepsLen: () => exports.get_steps_len(),
 			generateClasses: () => {
-				// LINK https://stackoverflow.com/questions/47529643/how-to-return-a-string-or-similar-from-rust-in-webassembly
-				// LINK https://stackoverflow.com/questions/49014610/passing-a-javascript-string-to-a-rust-function-compiled-to-webassembly/49020435#49020435
-				const height = maze.get_height();
-				const width = maze.get_width();
-				const ptr = maze.get_final();
+				const height = exports.get_height();
+				const width = exports.get_width();
+				const ptr = exports.get_final();
 				console.log('class lists generated');
 				const classPtrs = Array.from(new Uint32Array(memory.buffer, ptr, height));
 				return classPtrs.map((p) =>
@@ -35,18 +32,32 @@ export const getApi = async (): Promise<WASMApi> => {
 	})();
 }
 
-async function loadWasm() {
-	let { module, instance } = await fetch('rs')
+// LINK https://stackoverflow.com/questions/47529643/how-to-return-a-string-or-similar-from-rust-in-webassembly
+// LINK https://stackoverflow.com/questions/49014610/passing-a-javascript-string-to-a-rust-function-compiled-to-webassembly/49020435#49020435
+type WasmModule = {
+	module: WebAssembly.Module;
+	exports: MazeDescriptor;
+	memory: WebAssembly.Memory;
+}
+async function loadWasm(): Promise<WasmModule> {
+	let { module, instance: { exports } } = await fetch('rs')
 		.then(response => response.arrayBuffer())
 		.then(bytes => WebAssembly.instantiate(bytes, { env: wasmImports }))
 		.then(results => {
 			console.log(results);
 			return results;
 		});
-	const memory = instance.exports.memory as WebAssembly.Memory;
+	const memory = exports.memory as WebAssembly.Memory;
+	const { create_maze, get_height, get_width, get_final, get_steps_len } = exports;
 	return {
-		module, 
-		instance,
+		module,
+		exports: {
+			create_maze: (width: number, height: number) => (create_maze as CallableFunction)(width, height),
+			get_width: get_width as () => number,
+			get_height: get_height as () => number,
+			get_final: get_final as () => number,
+			get_steps_len: get_steps_len as () => number,
+		},
 		memory
 	}
 }
