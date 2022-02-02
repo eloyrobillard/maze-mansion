93ccbd30948e502064cf88928450ddccd3dc062f
eloyrobillard <eloy.robillard@gmail.com>
2022-02-02 10:46:04 +0100

    🦄 refactor(maze_descriptor): libから抽出

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,75 +1,16 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::sync::Mutex;
+mod maze_descriptor;
 mod maze_utils;
 mod rand;
 
+use maze_descriptor::MazeDescriptor;
 use maze_utils::*;
-use rand::{gen};
+use std::sync::Mutex;
 
 lazy_static! {
     static ref MAZE_DESCRIPTOR: Mutex<MazeDescriptor> = Mutex::new(MazeDescriptor::new(10, 10));
-}
-
-#[repr(C)]
-pub struct MazeDescriptor {
-    height: usize,
-    width: usize,
-    steps: Vec<Vec<i32>>,
-    fin: Vec<Vec<i32>>,
-}
-
-impl MazeDescriptor {
-    pub fn new(width: usize, height: usize) -> MazeDescriptor {
-        let invalid = -1;
-
-        let mut grid: Vec<Vec<i32>> = init_grid(width, height);
-
-        let mut cell_stack: Vec<i32> = Vec::new();
-
-        let x: usize = gen(width-1);
-        let y: usize = gen(height-1);
-
-        let mut prev = invalid;
-        let mut current = grid[y][x] + get_neighbors(&mut grid, x, y);
-
-        cell_stack.push(current);
-        let mut visited = 1;
-
-        let mut steps: Vec<Vec<i32>> = Vec::new();
-
-        let to_visit = width * height;
-
-        while visited < to_visit {
-            if current == invalid {
-                current = cell_stack.pop().unwrap();
-            }
-
-            steps.push(vec![prev, current]);
-
-            prev = current;
-            current = get_next(current, &mut grid);
-            prev = grid[get_y(prev) as usize][get_x(prev) as usize];
-            if current != invalid {
-                visited += 1;
-                current |= get_neighbors(&mut grid, get_x(current), get_y(current));
-                cell_stack.push(current)
-            }
-        }
-
-        steps.push(vec![prev, current]);
-        MazeDescriptor {
-            height,
-            width,
-            steps,
-            fin: grid,
-        }
-    }
-
-    pub fn reset(&mut self, width: usize, height: usize) {
-        *self = MazeDescriptor::new(width, height);
-    }
 }
 
 #[no_mangle]
---
+++ wasm/src/maze_descriptor.rs
@@ -0,0 +1,68 @@
+use crate::maze_utils::*;
+use crate::rand::{gen};
+
+pub struct MazeDescriptor {
+  pub height: usize,
+  pub width: usize,
+  pub steps: Vec<Vec<i32>>,
+  pub fin: Vec<Vec<i32>>,
+}
+
+impl MazeDescriptor {
+  pub fn new(width: usize, height: usize) -> MazeDescriptor {
+      let mut md = MazeDescriptor {
+          width: width,
+          height: width,
+          steps: Vec::new(),
+          fin: Vec::new(),
+      };
+
+      md.reset(width, height);
+      md
+  }
+
+  pub fn reset(&mut self, width: usize, height: usize) {
+      let invalid = -1;
+
+      let mut grid: Vec<Vec<i32>> = init_grid(width, height);
+
+      let mut cell_stack: Vec<i32> = Vec::new();
+
+      let x: usize = gen(width-1);
+      let y: usize = gen(height-1);
+
+      let mut prev = invalid;
+      let mut current = grid[y][x] + get_neighbors(&mut grid, x, y);
+
+      cell_stack.push(current);
+      let mut visited = 1;
+
+      let mut steps: Vec<Vec<i32>> = Vec::new();
+
+      let to_visit = width * height;
+
+      while visited < to_visit {
+          if current == invalid {
+              current = cell_stack.pop().unwrap();
+          }
+
+          steps.push(vec![prev, current]);
+
+          prev = current;
+          current = get_next(current, &mut grid);
+          prev = grid[get_y(prev) as usize][get_x(prev) as usize];
+          if current != invalid {
+              visited += 1;
+              current |= get_neighbors(&mut grid, get_x(current), get_y(current));
+              cell_stack.push(current)
+          }
+      }
+
+      steps.push(vec![prev, current]);
+
+      self.width = width;
+      self.height = height;
+      self.steps = steps;
+      self.fin = grid;
+  }
+}
