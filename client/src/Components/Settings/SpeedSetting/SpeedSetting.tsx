360f1c9b3dc364a979fba76a2f9cc103a39f93aa
eloyrobillard <eloy.robillard@gmail.com>
2022-02-10 17:13:57 +0100

    🦄 refactor(SpeedSetting): don't allow fps > 100

--- client/src/Components/Settings/SpeedSetting/SpeedSetting.tsx
+++ client/src/Components/Settings/SpeedSetting/SpeedSetting.tsx
@@ -1,22 +1,18 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { SettingsContext } from 'Dashboard';
 import './SpeedSettings.css';
 
+const maxFPS = 100;
+
 export default function SpeedSetting() {
-  const {mazeWidth, mazeHeight, fps, setFps} = useContext(SettingsContext);
-  const [max, setMax] = useState(100);
+  const {fps, setFps} = useContext(SettingsContext);
 
+  // NOTE update fps with new maxFPS
   useEffect(() => {
-    // NOTE 100 <= max <= 1000
-    setMax(Math.max(100, Math.min(mazeWidth * mazeHeight, 1000)));
-  }, [mazeWidth, mazeHeight]);
-
-  // NOTE update fps with new max
-  useEffect(() => {
-    if (fps > max) {
-      setFps(max);
+    if (fps > maxFPS) {
+      setFps(maxFPS);
     }
-  }, [max, fps, setFps]);
+  }, [fps, setFps]);
 
   function handleFps(e: React.ChangeEvent<HTMLInputElement>) {
     e.preventDefault();
@@ -30,45 +26,14 @@ export default function SpeedSetting() {
       <div id="indicators">
         <span>1</span>
         <span id="fps-span">{fps}</span>
-        <span>{max === 1000 ? '1K' : max}</span>
+        <span>{maxFPS}</span>
       </div>
       <input type="range"
         min="1"
-        max={max}
+        max={maxFPS}
         name="fps-range" 
         value={fps} 
         onChange={handleFps}/>
     </form>
   )
 }
-
-/* function SpeedToggle() {
-  const {setFps} = useContext(SettingsContext);
-  const [fpsElement, setFpsElement] = useState(
-    <form>
-      <label>レンダー速度</label>
-      <div id="speed-choice">
-        <button onClick={handleSpeedInstant}>瞬間的？</button>
-        <button onClick={handleSpeedSetting}>調整する？</button>
-      </div>
-    </form>
-  );
-
-  function handleSpeedInstant(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    e.preventDefault();
-    setFps(FPS_INSTANT);
-    setFpsElement(
-      <div>
-        <label>レンダー速度</label>
-        <h2>瞬間的</h2>
-      </div>
-    );
-  }
-
-  function handleSpeedSetting(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    e.preventDefault();
-    setFpsElement(<SpeedSetting />)
-  }
-
-  return fpsElement;
-} */
